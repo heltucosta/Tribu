f76cc318e4c3f64d0fcc1d9b2a9579b836f075bd
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-04-04 15:36:31 -0300

    ajuste fino formularios

--- Compensacoes/admin.py
+++ Compensacoes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Compensations)
+admin.site.register(CompensationTypes)
+admin.site.register(Selic)
--- Compensacoes/models.py
+++ Compensacoes/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Compensations(models.Model):
     name = models.CharField('Compensação', max_length=200)
-    date_start = models.DateTimeField('Data de início')
-    date_end = models.DateTimeField('Data fim')
+    date_start = models.DateField('Data de início')
+    date_end = models.DateField('Data fim')
 
 class CompensationTypes(models.Model):
     name = models.CharField('Tipo de compensação', max_length=200)
