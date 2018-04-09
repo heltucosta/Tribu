698d09e5b82c97b5838b25a588f8f3b869bb9d4b
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-04-09 11:36:56 -0300

    Criada home e pagina de clientes.

--- Businesses/views.py
+++ Businesses/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import *
 # Create your views here.
 def index(request):
-    return render(request, 'base.html', {})
+    clientes = Businesses.objects.all()
+    context = {'clientes': clientes}
+    return render(request, 'businesses/home.html', context)
+
+def cliente(request, client_id):
+    try:
+        cliente = Businesses.objects.get(id = client_id)
+    except:
+        return render(request, 'businesses/home.html', context)
+
+    context = {'cliente': cliente}
+    return render(request, 'businesses/cliente.html', context)
--- Tribu/urls.py
+++ Tribu/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from Businesses import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index) ,
     path('reajuste/', include('Compensacoes.urls')),
     path('businesses/', include('Businesses.urls')),
 ]
