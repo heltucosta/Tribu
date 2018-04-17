95d276464e4b3f1444bc3853ec609e5144ed5cff
Helton Costa <heltucosta@Heltons-MacBook-Air.local>
2018-04-17 11:10:16 -0300

    Adicionado heroku como allowed_host

--- Tribu/settings.py
+++ Tribu/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'bqe8j$*1tey-ms(z!514bnk-1m&b)r-(c-^#0(zab$se9##pp3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerful-retreat-52911.herokuapp.com']
 
 # Application definition
 
